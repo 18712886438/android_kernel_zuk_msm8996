35befc88debc36b856c5ccf761efc6f19e91f0db
Deepak Kumar <dkumar@codeaurora.org>
2016-02-01 19:48:46 +0530

    adreno_tz: Use context aware dcvs calls, if available
    Add support to use context aware dcvs, if it is supported
    by TZ. Context aware dcvs helps in handling sudden workload
    scenarios.
    
    Change-Id: I5e6e6003a5c61eacb4f5af91910994919a5a7684
    Signed-off-by: Deepak Kumar <dkumar@codeaurora.org>
    Signed-off-by: Divya Ponnusamy <pdivya@codeaurora.org>
    
    Conflicts:
    	drivers/devfreq/governor_msm_adreno_tz.c
    	include/linux/msm_adreno_devfreq.h

--- include/linux/msm_adreno_devfreq.h
+++ include/linux/msm_adreno_devfreq.h
@@ -32,6 +32,8 @@ struct devfreq_msm_adreno_tz_data {
 		int last_level;
 		int cycles_keeping_level;
 #endif
+		u32 ctxt_aware_target_pwrlevel;
+		u32 ctxt_aware_busy_penalty;
 	} bin;
 	struct {
 		u64 total_time;
@@ -51,6 +53,7 @@ struct devfreq_msm_adreno_tz_data {
 	unsigned int device_id;
 	bool is_64;
 	bool disable_busy_time_burst;
+	bool ctxt_aware_enable;
 };
 
 struct msm_adreno_extended_profile {
