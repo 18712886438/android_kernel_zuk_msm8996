d8761943e444fb7228fbc43bb9fd8e3c73ba2c6d
Daniel Rosenberg <drosen@google.com>
2016-02-11 16:44:15 -0800

    vfs: add d_canonical_path for stacked filesystem support
    Inotify does not currently know when a filesystem
    is acting as a wrapper around another fs. This means
    that inotify watchers will miss any modifications to
    the base file, as well as any made in a separate
    stacked fs that points to the same file.
    d_canonical_path solves this problem by allowing the fs
    to map a dentry to a path in the lower fs. Inotify
    can use it to find the appropriate place to watch to
    be informed of all changes to a file.
    
    Change-Id: I09563baffad1711a045e45c1bd0bd8713c2cc0b6
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    
    Conflicts:
    	include/linux/dcache.h

--- include/linux/dcache.h
+++ include/linux/dcache.h
@@ -160,6 +160,8 @@ struct dentry_operations {
 	char *(*d_dname)(struct dentry *, char *, int);
 	struct vfsmount *(*d_automount)(struct path *);
 	int (*d_manage)(struct dentry *, bool);
+	struct inode *(*d_select_inode)(struct dentry *, unsigned);
+	void (*d_canonical_path)(const struct dentry *, struct path *);
 } ____cacheline_aligned;
 
 /*
