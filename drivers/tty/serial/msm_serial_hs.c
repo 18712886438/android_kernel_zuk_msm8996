6d8c5838c7e7b78456731dcd207963cff56b1469
CallMESuper <zj7859256@gmail.com>
2016-08-24 01:13:05 +0800

    serial: msm_serial_hs: Move clk count voting out of runtime callbacks

--- drivers/tty/serial/msm_serial_hs.c
+++ drivers/tty/serial/msm_serial_hs.c
@@ -214,7 +214,7 @@ struct msm_hs_port {
 	struct clk *pclk;
 	struct msm_hs_tx tx;
 	struct msm_hs_rx rx;
-	atomic_t clk_count;
+	atomic_t resource_count;
 	struct msm_hs_wakeup wakeup;
 	struct wakeup_source ws;
 
@@ -378,7 +378,7 @@ static void msm_hs_clk_bus_unvote(struct msm_hs_port *msm_uport)
 static void msm_hs_resource_unvote(struct msm_hs_port *msm_uport)
 {
 	struct uart_port *uport = &(msm_uport->uport);
-	int rc = atomic_read(&msm_uport->clk_count);
+	int rc = atomic_read(&msm_uport->resource_count);
 
 	MSM_HS_DBG("%s(): power usage count %d", __func__, rc);
 	if (rc <= 0) {
@@ -386,7 +386,7 @@ static void msm_hs_resource_unvote(struct msm_hs_port *msm_uport)
 		WARN_ON(1);
 		return;
 	}
-	atomic_dec(&msm_uport->clk_count);
+	atomic_dec(&msm_uport->resource_count);
 	pm_runtime_mark_last_busy(uport->dev);
 	pm_runtime_put_autosuspend(uport->dev);
 }
@@ -403,7 +403,7 @@ static void msm_hs_resource_vote(struct msm_hs_port *msm_uport)
 		msm_hs_pm_resume(uport->dev);
 	}
 
-	atomic_inc(&msm_uport->clk_count);
+	atomic_inc(&msm_uport->resource_count);
 }
 
 /* Check if the uport line number matches with user id stored in pdata.
@@ -592,8 +592,8 @@ static void dump_uart_hs_registers(struct msm_hs_port *msm_uport)
 	struct uart_port *uport = &(msm_uport->uport);
 
 	if (msm_uport->pm_state != MSM_HS_PM_ACTIVE) {
-		MSM_HS_INFO("%s:Failed clocks are off, clk_count %d",
-			__func__, atomic_read(&msm_uport->clk_count));
+		MSM_HS_INFO("%s:Failed clocks are off, resource_count %d",
+			__func__, atomic_read(&msm_uport->resource_count));
 		return;
 	}
 
@@ -3603,9 +3603,9 @@ static void msm_hs_shutdown(struct uart_port *uport)
 			 UART_XMIT_SIZE, DMA_TO_DEVICE);
 
 	msm_hs_resource_unvote(msm_uport);
-	rc = atomic_read(&msm_uport->clk_count);
+	rc = atomic_read(&msm_uport->resource_count);
 	if (rc) {
-		atomic_set(&msm_uport->clk_count, 1);
+		atomic_set(&msm_uport->resource_count, 1);	
 		MSM_HS_WARN("%s(): removing extra vote\n", __func__);
 		msm_hs_resource_unvote(msm_uport);
 	}
