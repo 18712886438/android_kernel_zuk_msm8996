e59b85a9161eb82dbd31477c9eff607473c22e60
CallMESuper <zj7859256@gmail.com>
2017-01-09 21:31:28 +0800

    fix build tools

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 rm -rf modules
-export CONFIG_FILE="z2_plus_perf_defconfig"
+export CONFIG_FILE="cyanogenmod_z2_defconfig"
 export ARCH="arm64"
 export CROSS_COMPILE="aarch64-linux-android-"
 export TOOL_CHAIN_PATH="${HOME}/Mokee/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/"
 export CONFIG_ABS_PATH="arch/${ARCH}/configs/${CONFIG_FILE}"
 export PATH=$PATH:${TOOL_CHAIN_PATH}
-export objdir="${HOME}/kernel/obj"
-export sourcedir="${HOME}/kernel/z2p"
+export objdir="${HOME}/Mokee/kernel/zuk/obj"
+export sourcedir="${HOME}/Mokee/kernel/zuk/msm8996"
 compile() {
   make O=$objdir ARCH=arm64 CROSS_COMPILE=${TOOL_CHAIN_PATH}/${CROSS_COMPILE}  $CONFIG_FILE -j4 
   make O=$objdir -j6
