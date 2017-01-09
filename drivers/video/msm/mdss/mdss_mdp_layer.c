MotorolaMobilityLLC/kernel-msm

msm: mdss: Set the scaler flag in pipe after copying scaler data
While copying the scaler data from layer to pipe, the scaler flag
might get reset. Set the scalar flag after copying the scaler data.

Change-Id: I20bfef83109aadcac55e780b50b5fd1ea2302d3c
Signed-off-by: Krishna Chaitanya Devarakonda <kdevarak@codeaurora.org>

drivers/video/msm/mdss/mdss_mdp_layer.c
@@ -509,7 +509,6 @@ static int __configure_pipe_params(struct msm_fb_data_type *mfd,
 	if (layer->flags & MDP_LAYER_PP)
 		pipe->flags |= MDP_OVERLAY_PP_CFG_EN;
 
-	pipe->scaler.enable = (layer->flags & SCALER_ENABLED);
 	pipe->is_fg = layer->flags & MDP_LAYER_FORGROUND;
 	pipe->img_width = layer->buffer.width & 0x3fff;
 	pipe->img_height = layer->buffer.height & 0x3fff;
@@ -540,6 +539,12 @@ static int __configure_pipe_params(struct msm_fb_data_type *mfd,
 	pr_debug("src{%d,%d,%d,%d}, dst{%d,%d,%d,%d}\n",
 		pipe->src.x, pipe->src.y, pipe->src.w, pipe->src.h,
 		pipe->dst.x, pipe->dst.y, pipe->dst.w, pipe->dst.h);
+
+	if (layer->flags & SCALER_ENABLED)
+		memcpy(&pipe->scaler, layer->scale,
+			sizeof(struct mdp_scale_data_v2));
+
+	pipe->scaler.enable = (layer->flags & SCALER_ENABLED);
 
 	flags = pipe->flags;
 	if (is_single_layer)
@@ -661,9 +666,6 @@ static int __configure_pipe_params(struct msm_fb_data_type *mfd,
 		}
 	}
 
-	if (layer->flags & SCALER_ENABLED)
-		memcpy(&pipe->scaler, layer->scale,
-			sizeof(struct mdp_scale_data_v2));
 	ret = mdss_mdp_overlay_setup_scaling(pipe);
 	if (ret) {
 		pr_err("scaling setup failed %d\n", ret);

18712886438/android_kernel_zuk_msm8996

drivers/video/msm/mdss/mdss_mdp_layer.c
@@ -463,7 +463,6 @@ static int __configure_pipe_params(struct msm_fb_data_type *mfd,
 	if (layer->flags & MDP_LAYER_PP)
 		pipe->flags |= MDP_OVERLAY_PP_CFG_EN;
 
-	pipe->scale.enable_pxl_ext = layer->flags & MDP_LAYER_ENABLE_PIXEL_EXT;
 	pipe->is_fg = layer->flags & MDP_LAYER_FORGROUND;
 	pipe->img_width = layer->buffer.width & 0x3fff;
 	pipe->img_height = layer->buffer.height & 0x3fff;
@@ -493,6 +492,12 @@ static int __configure_pipe_params(struct msm_fb_data_type *mfd,
 	pr_debug("src{%d,%d,%d,%d}, dst{%d,%d,%d,%d}\n",
 		pipe->src.x, pipe->src.y, pipe->src.w, pipe->src.h,
 		pipe->dst.x, pipe->dst.y, pipe->dst.w, pipe->dst.h);
+
+	if (layer->flags & MDP_LAYER_ENABLE_PIXEL_EXT)
+		memcpy(&pipe->scale, layer->scale,
+			sizeof(struct mdp_scale_data));
+
+	pipe->scale.enable_pxl_ext = (layer->flags & MDP_LAYER_ENABLE_PIXEL_EXT);
 
 	flags = pipe->flags;
 	if (is_single_layer)
@@ -613,9 +618,6 @@ static int __configure_pipe_params(struct msm_fb_data_type *mfd,
 		}
 	}
 
-	if (layer->flags & MDP_LAYER_ENABLE_PIXEL_EXT)
-		memcpy(&pipe->scale, layer->scale,
-			sizeof(struct mdp_scale_data));
 	ret = mdss_mdp_overlay_setup_scaling(pipe);
 	if (ret) {
 		pr_err("scaling setup failed %d\n", ret);