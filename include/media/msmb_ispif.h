10dad097e252177a6b74ee806f8b778ae4270fca
CallMESuper <zj7859256@gmail.com>
2017-01-09 19:49:59 +0800

    Revert "msm: camera: uapi header split"
    This reverts commit 002f3a8bc5d8f1099e67226e3241f968a12e815f.

--- include/media/msmb_ispif.h
+++ include/media/msmb_ispif.h
@@ -1,9 +1,5 @@
-#ifndef UAPI_MSMB_ISPIF_H
-#define UAPI_MSMB_ISPIF_H
-
-#include <linux/types.h>
-#include <linux/ioctl.h>
-#include <linux/videodev2.h>
+#ifndef MSM_CAM_ISPIF_H
+#define MSM_CAM_ISPIF_H
 
 #define CSID_VERSION_V20                      0x02000011
 #define CSID_VERSION_V22                      0x02001000
